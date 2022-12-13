da6a73bb022b9a67fdb080a3c2bd149c6fe3e39d
Tolokoban <contact@tolokoban.org>
2022-12-13 18:44:31 +0100

    Improved Usage

--- src/args.ts
+++ src/args.ts
@@ -37,9 +37,25 @@ function usage(): never {
     console.log()
     console.log("Usage:")
     console.log(
-        Chalk.whiteBright.bold(
-            "ts-node-esm api.ts <protocol filename> [DOC <path>] [CLIENT <path>] [SERVER <path>]"
+        Chalk.yellowBright.bold(
+            "npx @tolokoban/api <protocol filename> [DOC <path>] [CLIENT <path>] [SERVER <path>]"
         )
+    )
+    console.log()
+    console.log(
+        "",
+        Chalk.yellow("<protocol filename>"),
+        "is a file describing the API in a subset of Typescript"
+    )
+    console.log(
+        "",
+        Chalk.yellow("CLIENT <path>"),
+        "if given, generates the Client code in <path>."
+    )
+    console.log(
+        "",
+        Chalk.yellow("SERVER <path>"),
+        "if given, generates the Server code in <path>."
     )
     console.log()
     process.exit(1)
