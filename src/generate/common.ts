11a5c70c1c0abd87a157444eff75543571ab377f
Tolokoban <contact@tolokoban.org>
2023-01-14 14:55:52 +0100

    Better error reports

--- src/generate/common.ts
+++ src/generate/common.ts
@@ -205,18 +205,18 @@ export function generateAsserts(types: Set<string>): CodeBlock {
         code.push(
             `function assertBoolean(data: unknown, prefix: string): asserts data is boolean {
     if (typeof data === "boolean") return
-    throw Error(\`\${prefix} was expected to be a Boolean!\`)
+    throwInvalidType(prefix, "Boolean", data)
 }`
         )
     if (types.has("number"))
         code.push(`function assertNumber(data: unknown, prefix: string): asserts data is number {
     if (typeof data === "number") return
-    throw Error(\`\${prefix} was expected to be a Number!\`)
+    throwInvalidType(prefix, "Number", data)
 }`)
     if (types.has("string"))
         code.push(`function assertString(data: unknown, prefix: string): asserts data is string {
     if (typeof data === "string") return
-    throw Error(\`\${prefix} was expected to be a String!\`)
+    throwInvalidType(prefix, "String", data)
 }`)
     if (types.has("object"))
         code.push(`function assertObject(
@@ -224,13 +224,23 @@ export function generateAsserts(types: Set<string>): CodeBlock {
     prefix: string
 ): asserts data is Record<string, unknown> {
     if (data && !Array.isArray(data) && typeof data === "object") return
-    throw Error(\`\${prefix} was expected to be an Object!\`)
+    throwInvalidType(prefix, "Object", data)
 }`)
     if (types.has("array"))
         code.push(`function assertArray(data: unknown, prefix: string): asserts data is unknown[] {
     if (Array.isArray(data)) return
-    throw Error(\`\${prefix} was expected to be an Array!\`)
+    throwInvalidType(prefix, "Array", data)
 }`)
+    if (code.length > 0) {
+        code.unshift(`function throwInvalidType(prefix: string, type: string, data: unknown): never {
+    const LIMIT = 40
+    let text = JSON.stringify(data) ?? "undefined"
+    if (text.length > LIMIT) {
+        text = \`\${text.substring(0, LIMIT - 3)}...\`
+    }
+    throw Error(\`\${prefix}'s type must be "\${type}" but we got \${text}!\`)
+}`)
+    }
     return code
 }
 
